1e09f58907d2838e2be124ca5c565fa23c1feb5b
Patrik Šíma <patrik@wrongware.cz>
2015-01-12 11:46:43 +0100

    remove docs

--- install/up.sql
+++
@@ -1,8 +0,0 @@
-SET NAMES utf8;
-SET foreign_key_checks = 0;
-SET time_zone = 'SYSTEM';
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-ALTER TABLE `xwt_helpdesk`
-ADD `prava` tinyint(1) unsigned NOT NULL DEFAULT '0',
-COMMENT='';
